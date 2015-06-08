4521234a7fab1fefb2e4fd05020ff04378316a77
PonyCui <cuiminghui@yy.com>
2015-06-08 17:11:56 +0800

    add demo project

---
+++ OCDebuggerDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OCDebuggerDemo
+//
+//  Created by 崔 明辉 on 15/6/8.
+//  Copyright (c) 2015年 PonyCui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ OCDebuggerDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  OCDebuggerDemo
+//
+//  Created by 崔 明辉 on 15/6/8.
+//  Copyright (c) 2015年 PonyCui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
